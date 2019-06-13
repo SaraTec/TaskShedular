708b94a68fa358372c4e973a390f3d97b65d67c9
Taras Boichuk <cosmo.epik@gmail.com>
2019-06-13 20:57:11 +0300

    Task Shedular

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: [], text: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+            <div>
+                <h3>Список справ</h3>
+                <TodoList items={this.state.items} />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-todo">
+                        Нова справ
+            </label>
+                    <input
+                        id="new-todo"
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                    />
+                    <button>
+                        Добавити #{this.state.items.length + 1}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.text.length) {
+            return;
+        }
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            text: ''
+        }));
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+render(
+    <TodoApp />,
+    document.getElementById('todos-example')
+);
